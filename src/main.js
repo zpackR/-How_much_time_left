3de86513f29920ff42bb7612fd7590b91fa431a6
Rafael <116080597+zpackR@users.noreply.github.com>
2024-04-26 13:57:28 -0300

    creating the main.js, and make yours changes

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const actalDate = new Date();
+let objectiveDate = prompt('Final date... Exemple: XXXX-XX-XX');
+
+objectiveDate = new Date(objectiveDate);
+let daysRemaining = Math.floor((objectiveDate-actalDate) / 86400000);
+
+document.querySelector('#days-remaining').textContent = daysRemaining;
