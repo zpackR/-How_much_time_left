0caadcb286c6d87687ba3eccaed94243e269359f
Rafael <116080597+zpackR@users.noreply.github.com>
2024-05-08 15:06:41 -0300

    changes on css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-const actalDate = new Date();
-let objectiveDate = prompt('Final date... Exemple: XXXX-XX-XX');
+// const actalDate = new Date();
+// let objectiveDate = prompt('Final date... Exemple: XXXX-XX-XX');
 
-objectiveDate = new Date(objectiveDate);
-let daysRemaining = Math.floor((objectiveDate-actalDate) / 86400000);
+// objectiveDate = new Date(objectiveDate);
+// let daysRemaining = Math.floor((objectiveDate-actalDate) / 86400000);
 
-document.querySelector('#days-remaining').textContent = daysRemaining;
+// document.querySelector('#days-remaining').textContent = daysRemaining;
