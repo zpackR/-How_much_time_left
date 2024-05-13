c75f2f9c4c1830e556a3d196d6d42c9c969e12ee
Rafael <116080597+zpackR@users.noreply.github.com>
2024-05-13 12:53:31 -0300

    Cont

--- src/main.js
+++ src/main.js
@@ -5,3 +5,10 @@
 // let daysRemaining = Math.floor((objectiveDate-actalDate) / 86400000);
 
 // document.querySelector('#days-remaining').textContent = daysRemaining;
+
+let idade = 45;
+let quadradoIdade = idade * idade
+let IdadeAnterior = idade - 20
+let calc = quadradoIdade - IdadeAnterior
+
+console.log(calc);
